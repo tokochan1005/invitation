2afa04855067ce7f2a960e84e5f70f15175f0641
t-nagata <t-nagata@a-tm.co.jp>
2022-07-25 00:00:02 +0900

    Initial Commit

---
+++ invitation/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  invitation
+//
+//  Created by t-nagata on 2022/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ invitation/invitationApp.swift
@@ -0,0 +1,17 @@
+//
+//  invitationApp.swift
+//  invitation
+//
+//  Created by t-nagata on 2022/07/24.
+//
+
+import SwiftUI
+
+@main
+struct invitationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
