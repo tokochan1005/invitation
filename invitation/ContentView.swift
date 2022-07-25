b5aee0d04c27302fb7ad9e4aeeffc693d1be8f8d
wrsdu1715 <wrsdu1715@gmail.com>
2022-07-25 12:26:11 +0900

    test

--- invitation/ContentView.swift
+++ invitation/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
         VStack {
             TabBarView(selectedTab: $selectedTab)
             TabView (selection: $selectedTab) {
-                PageView(text: "ホーム")
+                PageView(text: "ホームdesu")
                     .tag(1)
                 PageView(text: "編集")
                     .tag(2)
